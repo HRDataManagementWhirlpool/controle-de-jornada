5eb0f9c6171f2c5c659597e71cb266275a7a0927
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-01-12 14:05:09 -0300

    Projeto Descontinuado

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from controllers.selenium import SeleniumController
 from controllers.ronda import RondaController
 from controllers.workforce import WorkforceController
-from views import main
 
+iniciar_selenium = SeleniumController.iniciar_acesso()
--- src/controllers/selenium.py
+++ src/controllers/selenium.py
@@ -8,15 +8,15 @@ import glob
 import subprocess
 from dotenv import load_dotenv
 
+driver = webdriver.Chrome()
 inicio = time.time()
 load_dotenv()
 
 class SeleniumController:
     def __init__(self):
-        driver = webdriver.Chrome()
-        
         driver.get("https://logincloud.senior.com.br/logon/LogonPoint/tmindex.html")
         
+    def iniciar_acesso():
         try:
             element = WebDriverWait(driver, 10).until( #espera até 10s para que o elemento especificado apareça
                 EC.presence_of_element_located((By.XPATH, "//input[@id='login']"))
@@ -24,8 +24,8 @@ class SeleniumController:
         except Exception as e:
             print(f"Erro ao tentar executar código: {e}")
         else:
-            loginSenior = driver.find_element(By.XPATH, "//input[@id='login']")
-            loginSenior.send_keys(os.getenv('LOGIN'))
+            login = driver.find_element(By.XPATH, "//input[@id='login']")
+            login.send_keys(os.getenv('LOGIN'))
             
             ### Clicar em Log On
             logOnBtn = driver.find_element(By.ID, "loginBtn")
@@ -45,25 +45,28 @@ class SeleniumController:
                 ### Clicar em Log On novamente
                 logOnBtn = driver.find_element(By.ID, "loginBtn")
                 logOnBtn.click()
-                
+            ## Função de Login termina aqui
                 ### Ao logar Clicar em Acesso e Segurança - Produção
-                try:
-                    element = WebDriverWait(driver, 10).until( #espera até 10s para que o elemento especificado apareça
-                        EC.presence_of_element_located((By.CLASS_NAME, "storeapp-details-link"))
-                    )
-                except Exception as e:
-                    print(f"Erro ao tentar executar código: {e}")
-                else:
-                    acesso = driver.find_element(By.CLASS_NAME, "storeapp-details-link")
-                    acesso.click()
-                    
-                    ### Ao clicar, irá baixar um arquivo. Esse arquivo precisa ser clicado para abrir o sistema
-                    time.sleep(2) # Espera o arquivo ser baixado          
-                    lista_arquivos= glob.glob("C:/Users/DESOUR10/Downloads/*") #pegando todos os arquivos dentro da pasta downloads
-                    ultimo_arquivo=max(lista_arquivos, key=os.path.getmtime) #armazenando quem tem a data mais recente dentro da pasta
-                    driver.quit()
-                    try:
-                        # Executa o arquivo usando subprocess
-                        subprocess.run(ultimo_arquivo, shell=True)
-                    except Exception as e:
-                        print(f"Erro ao tentar executar código: {e}")
+    ## Será uma função para abrir o arquivo de acesso ao RONDA
+    def iniciar_sistema(self):
+        try:
+            element = WebDriverWait(driver, 10).until( #espera até 10s para que o elemento especificado apareça
+                EC.presence_of_element_located((By.CLASS_NAME, "storeapp-details-link"))
+            )
+        except Exception as e:
+            print(f"Erro ao tentar executar código: {e}")
+        else:
+            acesso = driver.find_element(By.CLASS_NAME, "storeapp-details-link")
+            acesso.click()
+            
+            ### Ao clicar, irá baixar um arquivo. Esse arquivo precisa ser clicado para abrir o sistema
+            time.sleep(2) # Espera o arquivo ser baixado          
+            lista_arquivos= glob.glob("C:/Users/DESOUR10/Downloads/*") #pegando todos os arquivos dentro da pasta downloads
+            ultimo_arquivo=max(lista_arquivos, key=os.path.getmtime) #armazenando quem tem a data mais recente dentro da pasta
+            driver.quit()
+            try:
+                # Executa o arquivo usando subprocess
+                subprocess.run(ultimo_arquivo, shell=True)
+            except Exception as e:
+                print(f"Erro ao tentar executar código: {e}")
+            ## Termina a função de abertura do RONDA
